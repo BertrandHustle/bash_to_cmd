b5fee22b29b2470f322aaa3d30c8feac91ecc8b1
BertrandHustle <MFOrangeSlice@gmail.com>
2017-04-17 16:26:37 -0400

    added views

--- bash_to_cmd/bash_to_cmd_app/views.py
+++ bash_to_cmd/bash_to_cmd_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. This is the bash_to_cmd index.")
+
+def bash_translation_results(request, cmd_id):
+    return HttpResponse(reponse)
+
+def cmd_translation_results(request, bash_id):
+    return HttpResponse(reponse)
